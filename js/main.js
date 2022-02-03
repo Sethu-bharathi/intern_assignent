4d0e96aadd0285b5d2473adaa99bce1c88f27648
Sethu-bharathi <bharathisethu018@gmail.com>
2022-02-03 17:44:53 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-let btn=document.querySelector('.btn');
+let menutoggler=document.querySelector(".toggle");
+let mobilenav=document.querySelector(".mobilenavcontainer");
+
+menutoggler.onclick=function(){
+  menutoggler.classList.toggle("active");
+  mobilenav.classList.toggle("active");
+}
+
+let anav=document.querySelectorAll(".anav");
+
+for (let i = 0; i < anav.length; i++) {
+  anav[i].addEventListener("click",()=>{
+    menutoggler.classList.toggle("active");
+    mobilenav.classList.toggle("active");
+  })
+}
